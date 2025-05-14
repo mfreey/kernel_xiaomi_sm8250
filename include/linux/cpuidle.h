kerneltoast/android_kernel_google_zuma

cpuidle: Don't return from inside __CPU_PM_CPU_IDLE_ENTER() macro
Altering control flow from inside a macro is a recipe for disaster. This is
a nightmare for readability and makes it very easy to trip over the return
nestled in the __CPU_PM_CPU_IDLE_ENTER() macro.

Restructure __CPU_PM_CPU_IDLE_ENTER() to eliminate the nasty return.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

include/linux/cpuidle.h
@@ -288,15 +288,14 @@ extern s64 cpuidle_governor_latency_req(unsigned int cpu);
 									\
 	if (!idx) {							\
 		cpu_do_idle();						\
-		return idx;						\
-	}								\
-									\
-	if (!is_retention)						\
-		__ret =  cpu_pm_enter();				\
-	if (!__ret) {							\
-		__ret = low_level_idle_enter(state);			\
+	} else {							\
 		if (!is_retention)					\
-			cpu_pm_exit();					\
+			__ret = cpu_pm_enter();				\
+		if (!__ret) {						\
+			__ret = low_level_idle_enter(state);		\
+			if (!is_retention)				\
+				cpu_pm_exit();				\
+		}							\
 	}								\
 									\
 	__ret ? -1 : idx;						\

mfreey/kernel_xiaomi_sm8250

include/linux/cpuidle.h
@@ -276,15 +276,14 @@ static inline int cpuidle_register_governor(struct cpuidle_governor *gov)
 									\
 	if (!idx) {							\
 		cpu_do_idle();						\
-		return idx;						\
-	}								\
-									\
-	if (!is_retention)						\
-		__ret =  cpu_pm_enter();				\
-	if (!__ret) {							\
-		__ret = low_level_idle_enter(idx);			\
+	} else {							\
 		if (!is_retention)					\
-			cpu_pm_exit();					\
+			__ret = cpu_pm_enter();				\
+		if (!__ret) {						\
+			__ret = low_level_idle_enter(idx);		\
+			if (!is_retention)				\
+				cpu_pm_exit();				\
+		}							\
 	}								\
 									\
 	__ret ? -1 : idx;						\